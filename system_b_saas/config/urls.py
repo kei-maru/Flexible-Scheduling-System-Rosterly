104cc3704c47bdd3efb4a4db1bbd7fbb97607d08
kei-maru <2894758587@qq.com>
2026-02-28 23:52:32 +0900

    update_document

--- system_b_saas/config/urls.py
+++ system_b_saas/config/urls.py
@@ -1,8 +1,8 @@
 # system_b/config/urls.py
 from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.static import static
+from django.urls import path, include, re_path
 from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -19,4 +19,7 @@ urlpatterns = [
 ]
 
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
+]
